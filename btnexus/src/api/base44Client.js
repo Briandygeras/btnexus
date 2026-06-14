0e8355348dd34ef47da5d49682a270ef4059f4ee
Briandygeras <enzoa8891@gmail.com>
2026-06-14 16:37:27 -0300

    Update base44Client to use environment variable for URL

--- btnexus/src/api/base44Client.js
+++ btnexus/src/api/base44Client.js
@@ -3,12 +3,12 @@ import { appParams } from '@/lib/app-params';
 
 const { appId, token, functionsVersion, appBaseUrl } = appParams;
 
-//Create a client with authentication required
+// Create a client with authentication required
 export const base44 = createClient({
     appId,
     token,
     functionsVersion,
-    serverUrl: '',
+    serverUrl: import.meta.env.VITE_BASE44_APP_BASE_URL || appBaseUrl,
     requiresAuth: false,
     appBaseUrl
 });
