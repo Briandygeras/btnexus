d79a7bc784c1c6cfbfe5a0b52eb5bc9ed0551b97
Briandygeras <enzoa8891@gmail.com>
2026-06-18 15:51:33 -0300

    Refactor App component and add ScrollToTop

--- btnexus/src/App.jsx
+++ btnexus/src/App.jsx
@@ -4,6 +4,7 @@ import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
+import ScrollToTop from './components/sections/ScrollToTop.jsx';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import Home from './pages/Home';
 
